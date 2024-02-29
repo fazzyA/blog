2cffbb6941caf3ae7b67e3e1efe00cecc1c4ca68
fazzyA <faz.pak@gmail.com>
2024-02-29 10:41:43 +0500

    blod detail page and api calling

--- src/BlogDetail.js
+++ src/BlogDetail.js
@@ -1,13 +1,32 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import Header from './Header'
+import axios from 'axios'
+import { Card } from 'react-bootstrap'
 
 const BlogDetail = () => {
   const { id } = useParams() 
+  const [blog, setBlog] = useState({})
+
+  useEffect(()=>{
+    axios.get(`https://jsonplaceholder.typicode.com/posts/${id}`)
+    .then((data) => {
+      console.log(data.data)
+      setBlog(data.data)
+    })
+
+  }, [])
   return (
     <div>
       <Header />
-      blog detail - {id}
+      <Card>
+            <Card.Header className="text-capitalize">{blog.title && blog.title}</Card.Header>
+            <Card.Body>
+              <Card.Title>Blog - {blog.id}</Card.Title>
+              <Card.Text>{blog.body}</Card.Text>
+            </Card.Body>
+          </Card>
+
     </div>
   )
 }
--- src/BlogList.js
+++ src/BlogList.js
@@ -4,12 +4,12 @@ import { Link } from "react-router-dom"
 const BlogList = ({blogs}) => {
   return (
     <div>
-      {blogs.map((item) => (
+      {blogs && blogs.map((item) => (
             <Card>
             <Card.Header className="text-capitalize">{item.title}</Card.Header>
             <Card.Body>
-              <Card.Title>{item.author}</Card.Title>
-              <Card.Text>{item.detail.slice(0, 100)}...</Card.Text>
+              <Card.Title>Blog - {item.id}</Card.Title>
+              <Card.Text>{item.body.slice(0, 100)}...</Card.Text>
               <Button variant="primary">
                 <Link to={`blogs/${item.id}`} className="text-light text-decoration-none ">Read More...</Link>
                 </Button>
